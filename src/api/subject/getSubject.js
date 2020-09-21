c6c9aec2f558cc450fb054dac0fe6745c9a80319
FantasticYing <30038122+FantasticYing@users.noreply.github.com>
2020-09-21 22:19:09 +0800

    '时间格式化'

---
+++ src/api/subject/getSubject.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+class Params {
+  pageNum=1;
+  perPage=10;
+  personal=false
+}
+
+export const initParams = function () {
+  return new Params()
+}
+
+export function request (params) {
+  return axios.get('api/v1/subjects', {
+    params: params
+  })
+}
+
+export default { initParams, request }
