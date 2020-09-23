873cf62b4442853af0956f114a4ae670b8665963
TianZY <1438472761@qq.com>
2020-09-23 18:19:33 +0800

    考试计划

--- src/components/exam.vue
+++ src/components/exam.vue
@@ -5,62 +5,35 @@
       <el-breadcrumb-item>考试列表</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
-      <el-table
-      width=”100%“
-      :data="tableData"
-      border
-      :header-cell-style="[{height: '20px'}]"
-      >
-        <el-table-column
-          prop="date"
-          label="日期">
-        </el-table-column>
-        <el-table-column
-          prop="subject"
-          label="科目">
-        </el-table-column>
-        <el-table-column
-          prop="start_time"
-          label="开始时间">
-        </el-table-column>
-        <el-table-column
-          prop="end_time"
-          label="结束时间">
-        </el-table-column>
-        <el-table-column
-          label="操作">
-          <el-button>进入考试</el-button>
-        </el-table-column>
-      </el-table>
+      <paginate-table :data-list="examList" :pagination="pagination">
+        <el-table-column prop="subjectName" label="科目名称"></el-table-column>
+        <el-table-column prop="startTime" label="开始时间" :formatter="dateTimeFormatter"></el-table-column>
+        <el-table-column prop="endTime" label="结束时间" :formatter="dateTimeFormatter"></el-table-column>
+        <el-table-column prop="paperTitle" label="试卷标题"></el-table-column>
+      </paginate-table>
     </el-card>
   </div>
 </template>
 
 <script>
+// import { errorTip } from '../utils/tips'
+// import store from '../store'
+import { dateTimeFormatter } from '../utils/time'
+import PaginateTable from './PaginateTable'
+import store from '../store'
+
 export default {
-  name: 'exam',
+  name: 'examPlan',
+  components: { PaginateTable },
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        subject: '考啥试',
-        start_time: '21:30',
-        end_time: '22:30'
-      },
-      {
-        date: '2016-05-02',
-        subject: '啥考试',
-        start_time: '21:30',
-        end_time: '22:30'
-      },
-      {
-        date: '2016-05-02',
-        subject: '考试啥',
-        start_time: '21:30',
-        end_time: '22:30'
-      }
-      ]
+      state: store.state,
+      pagination: {},
+      dateTimeFormatter: dateTimeFormatter,
+      examList: []
     }
+  },
+  methods: {
   }
 }
 </script>
